8122986ef9c1becb937294c9d5e2c2965a9de428
dhruveshrana22 <161826216+dhruveshrana22@users.noreply.github.com>
2024-08-23 16:50:22 +0530

    Update again server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = 5000;
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://kalpesh/KalpeshVypar', {
+mongoose.connect('mongodb://0.0.0.0:27017/KalpeshVypar', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
